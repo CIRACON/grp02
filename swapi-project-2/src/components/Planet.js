de4b9ab2b1085733db845103bbeeed9ff37054ca
Bob Smith <wasadmin@webagesolutions.com>
2023-02-02 12:10:37 -0500

    added planets

---
+++ swapi-project-2/src/components/Planet.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from  "react";
+
+
+
+const Planet = () => {
+
+  const [planet, setPlanet] = useState({})
+
+  useEffect(()=>{
+    fetch(`https://swapi.dev/api/planets/1/`)
+    .then(re => re.json())
+    .then((re) => {
+      setPlanet(re.data);
+
+  })
+},[]);
+
+    return (
+      console.log(planet.name)
+
+
+
+  )
+}
+
+export default Planet
