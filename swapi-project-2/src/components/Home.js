5ac0f72eeb8464003695625926ac8c347a5234d5
Bob Smith <wasadmin@webagesolutions.com>
2023-02-07 17:11:45 -0500

    homepage css added

--- swapi-project-2/src/components/Home.js
+++ swapi-project-2/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import "./home.css"
+
 
 
 
@@ -11,13 +13,24 @@ export default function Home() {
   let activeClassName = "underline";
 
   return (
-    <nav>
-      <NavLink
-        to="people"
-        style={({ isActive }) => isActive ? activeStyle : undefined}>People
-      </NavLink>
-      
-    </nav>
+    <div className="homeDiv">
+      <div className="container">
+        <div className="red">
+          <h1>Star Wars Lookup </h1>
+        </div>
+        <div className="red">
+          <h4>Begin your Star Wars journey to a long time ago in a galaxy far, far away... </h4>
+        </div>
+        <div className="green">
+          <nav>
+          <NavLink
+            className="homeNavLink"
+            to="people"> Click to Start !
+          </NavLink>
+          </nav>
+        </div>
+      </div>
+    </div>
   )
 
 }
