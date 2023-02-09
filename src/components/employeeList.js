de22bd0a9cf878acc4075bbeb483419a78a96490
Bob Smith <wasadmin@webagesolutions.com>
2023-02-09 13:19:31 -0500

    search working

--- src/components/employeeList.js
+++ src/components/employeeList.js
@@ -1,8 +1,16 @@
 import React, {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
 
 export default function EmployeeList() {
 
-  const [employeeList,setEmployeeList] = useState([])
+
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [employeeList,setEmployeeList] = useState([]);
+  const handleChange = event => {
+    setSearchInput(event.target.value);
+  };
+
   let employeeDisplay = []
 
   useEffect(() => {
@@ -13,19 +21,32 @@ export default function EmployeeList() {
       employeeDisplay.push(employee))})
       .then(()=> setEmployeeList(employeeDisplay))
     .catch(err => console.error(err))
-  }, [])
+  }, []);
+
+  useEffect(()=>
+  {const results = employeeList.filter(employee =>
+    employee.name.toLowerCase().includes(searchInput)
+    );
+    setSearchResults(results);
+  }, [searchInput])
 
 
 
   return (
-    <div>
+    <div className="container">
       <h1>Employee Lookup</h1>
-      <ol>{employeeList.map(employee => (
-        <div>
-         <li> {employee.name} </li>
+      <input
+        type="text"
+        placeholder="Name Search"
+        value={searchInput}
+        onChange={handleChange}
+        />
+      {searchResults.map(employee => (
+        <div key={employee._id}>
+         <Link to={`${employee._id}`}> {employee.name} </Link>
         </div>
       )
-        )}</ol>
+        )}
     </div>
   )
 
