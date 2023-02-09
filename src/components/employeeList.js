531be8b300a6c8eacb614c321f55326c49a3a3df
Bob Smith <wasadmin@webagesolutions.com>
2023-02-09 10:09:18 -0500

    attempting render

--- src/components/employeeList.js
+++ src/components/employeeList.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 export default function EmployeeList() {
+
+  const [employeeList,setEmployeeList] = useState([])
+
+  useEffect(() => {
+  fetch('http://localhost:4000/employees')
+    .then((res) => res.json())
+    .then((employees) => {console.log(employees); return employees})
+    .then((res) => setEmployeeList(res))
+    .catch(err => console.error(err))
+  }, [])
+
   return (
     <div>
       <h1>Employee Lookup</h1>
+      <ul>{employeeList}</ul>
     </div>
   )
 
