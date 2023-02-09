5ea748c536e894ed8baca171686b4f113157437e
Bob Smith <wasadmin@webagesolutions.com>
2023-02-09 17:19:08 -0500

    serve it up

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const dao = require("./data_access");
 const cors = require('cors')
 const app = express();
-
+const path = require('path')
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname,'build')))
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'build','index.html'))
+});
 
 app.get("/employees", async (req, res) => {
     await dao.call('getAllEmployees', {}, (result) => {
