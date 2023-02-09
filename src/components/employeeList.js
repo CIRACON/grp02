e790cb8cbc03181047be21f31832e1f080df41c9
Bob Smith <wasadmin@webagesolutions.com>
2023-02-09 10:54:14 -0500

    employee list render

--- src/components/employeeList.js
+++ src/components/employeeList.js
@@ -3,19 +3,29 @@ import React, {useState, useEffect} from "react";
 export default function EmployeeList() {
 
   const [employeeList,setEmployeeList] = useState([])
+  let employeeDisplay = []
 
   useEffect(() => {
   fetch('http://localhost:4000/employees')
     .then((res) => res.json())
     .then((employees) => {console.log(employees); return employees})
-    .then((res) => setEmployeeList(res))
+    .then((res) =>{res.forEach((employee)=>
+      employeeDisplay.push(employee))})
+      .then(()=> setEmployeeList(employeeDisplay))
     .catch(err => console.error(err))
   }, [])
+
+
 
   return (
     <div>
       <h1>Employee Lookup</h1>
-      <ul>{employeeList}</ul>
+      <ol>{employeeList.map(employee => (
+        <div>
+         <li> {employee.name} </li>
+        </div>
+      )
+        )}</ol>
     </div>
   )
 
